12393bd1ab57290a03c03c31642c7b0e0c72aecf
Setland34 <waymakerijn@outlook.com>
2024-12-13 00:05:27 -0600

    commit

--- .deps/remix-tests/remix_accounts.sol
+++ .deps/remix-tests/remix_accounts.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
-// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
 contract RemixAccounts {
     struct AccountDetails{
-       address accountAddress;
-      uint256 balance;  
+       uint256 accountAddress;
+      address balance;  
    }
 
-    function getAccount(uint index) public pure returns (address, AccountDetails memory){
-        return 0x000... , AccountDetails({accountAddress: 0x...,balance: 1});
+     function getAccount(uint index) public pure returns (address){
+        return 0x6b4f9a2d5e1c3d7be78B69F47A8C52E37D86;// changed the type of your struct
      }
+}
