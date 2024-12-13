7d4e8c4cfec1d79134c26b4fd1bcab0c2db8f3fc
Setland34 <waymakerijn@outlook.com>
2024-12-12 23:33:56 -0600

    commit

--- .deps/remix-tests/remix_accounts.sol
+++ .deps/remix-tests/remix_accounts.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
 
-pragma solidity >=0.4.22 <0.9.0;
+contract RemixAccounts {
+    struct AccountDetails{
+       address accountAddress;
+      uint256 balance;  
+   }
 
-library TestsAccounts {
-    function getAccount(uint index) pure public returns (address) {
-        return address(0);
-    }
-}
+    function getAccount(uint index) public pure returns (address, AccountDetails memory){
+        return 0x000... , AccountDetails({accountAddress: 0x...,balance: 1});
+     }
